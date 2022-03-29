5cac6db6f22dc3ae62e96826f93e2344b1520059
Wojciech Skirło <34141085+WojciechSkirlo@users.noreply.github.com>
2022-03-29 18:24:47 +0100

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +8,30 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            //foreach (var student in Students)
+            //{
+            //    if()
+            //    Console.WriteLine("{0} : {1} : {2}", student.Name, student.Type, student.Enrollment);
+            //}
+
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+
         }
     }
 }
